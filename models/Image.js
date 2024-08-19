8d0fa7d331a39725873f5463c8da8e1a770ee8c3
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-18 21:55:46 -0500

    Added objects to models/Image.js

--- models/Image.js
+++ models/Image.js
@@ -11,7 +11,24 @@ Image.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        
+        createdBy: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        imageData: {
+            type: DataTypes.STRING,     // We are not sure yet, but it MIGHT be STRING
+            allowNull: false,
+        },
+        sessionId: {
+            type: DataTypes.INTEGER,    // References gameSession.id
+            allowNull: false,
+        },
+        votes: {    // To be tallied when gameSession.votingEnabled is true for each user that clicks the vote button
+                    // To be added to a player's vote count after voting has stopped and a winner is declared for a game session
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        },
     },
     {
         sequelize,
