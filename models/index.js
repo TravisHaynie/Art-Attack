904a66b2cc3aa3b4ad55258d81e4d9da1e8bf613
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-18 21:58:59 -0500

    Imported models to models/index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,7 @@
 const User = require("./User");
-const gameSession = require("./Session.js")
+const Subject = require("./Subject.js");
+const GameSession = require("./GameSession.js");
+const Image = require("./Image.js");
 
-module.exports = { User, gameSession };
+
+module.exports = { User, Subject, GameSession, Image }; // All models listed in order of dependence
