f148893e60f7bfe86d41472f65613e9e86ab769c
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-18 21:48:01 -0500

    Added new file models/Image.js and initial code.

---
+++ models/Image.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Image extends Model {}
+
+Image.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'image',
+    }
+);
+
+module.exports = Image;
