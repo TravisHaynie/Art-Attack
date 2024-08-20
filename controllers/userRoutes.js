968663e61bab8af13baf8b27e646a93d0da8bd30
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-19 19:39:27 -0500

    Added create user to userRoutes.js

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -40,6 +40,28 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// CREATE new user
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+
 // Logout route
 router.post('/logout', (req, res) => {
   try {
