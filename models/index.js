3d694d192804407b933a8460fcb85e07e48e85ee
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-19 19:53:25 -0500

    Added user/subject relationship in models/index.js

--- models/index.js
+++ models/index.js
@@ -3,7 +3,15 @@ const Subject = require("./Subject.js");
 const GameSession = require("./GameSession.js");
 const Image = require("./Image.js");
 
-Subject.belongs
+Subject.belongsTo(User, {
+    foreignKey: 'id',
+});
+
+User.hasMany(Subject, {
+    foreignKey: 'id',
+});
+
+
 
 
 module.exports = { User, Subject, GameSession, Image }; // All models listed in order of dependence
