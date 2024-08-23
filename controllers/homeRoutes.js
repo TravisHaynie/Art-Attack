bcf59b8b2c9349213fc0a5adb3671e93223b501a
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-22 19:46:14 -0500

    Added post route for game session in homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../models'); 
+const { User, GameSession } = require('../models'); 
 
 // Render homepage
 router.get('/', (req, res) => {
@@ -29,5 +29,59 @@ router.get('/canvas', (req, res) => {
         siteTitle: 'Vote Screen'
     });
 });
+
+router.post('/game-session', async (req, res) => {
+  // Check if player1, player2, and subject are provided in the request body
+  if (!req.body.player1 || !req.body.player2 || !req.body.subject) {
+    return res.status(400).json({ error: 'player1, player2, and subject are required fields' });
+  }
+
+  // Find the total count of subjects in the database
+  const totalSubjects = await Subject.count();
+
+  // Generate a random number between 1 and the total number of subjects
+  const randomSubjectId = Math.floor(Math.random() * totalSubjects) + 1;
+
+  // Find a subject based on the random subject ID
+  const subject = await Subject.findByPk(randomSubjectId);
+
+  // Check if player1 and player2 are different users
+  if (player1 === player2) {
+    return res.status(400).json({ error: 'player1 and player2 must be different users' });
+  }
+
+  try {
+    const newGameSession = await GameSession.create({
+      player1: req.user.id,
+      player2,
+      subject: subject.id,
+      inProgress: false,
+      votingEnabled: false,
+      hasVoted: null,
+    });
+
+    const gameSessionData = await GameSession.findByPk(newGameSession.id);
+    const gameSession = gameSessionData.get({ plain: true });
+
+    res.redirect(`/game-session/${gameSession.id}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+  // create game session
+
+  //take game session id and redirect to game-session/:id
+
+
+
+// game-session:id
+
+//game-session:id:player1
+
+//game-session:id:player2
+
+
 
 module.exports = router;
