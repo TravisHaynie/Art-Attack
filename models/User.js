561bcdb65d18cecf8df38cb66127bbd4846e0ff2
unknown <travihay@gmail.com>
2024-08-23 21:32:25 -0500

    added lobby.js and lobby.handles bars. changed routes for this to work. then chaned models layout

--- models/User.js
+++ models/User.js
@@ -52,6 +52,12 @@ User.init(
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
+      beforeUpdate: async (updatedUserData) => {
+        if (updatedUserData.password) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        }
+        return updatedUserData;
+      },
     },
     sequelize,
     timestamps: false,
