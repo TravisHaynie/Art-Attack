70185007f6279080cb76fd9fd8047ee01351f046
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-26 19:08:25 -0500

    Cleaning up controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const userRoutes = require('./userRoutes');
+const gameController = require('./game-controller');
 // const gameSessionApi = require('./api/gameSessionRoutes')
 
 // Use routes
 router.use('/', homeRoutes);
 router.use('/user', userRoutes);
+router.use('/api/game-controller', gameController)
 
 
 
